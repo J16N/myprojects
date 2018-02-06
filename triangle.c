763ae464b6d3dbbf0c2fd2a9d6d221519f10af73
Jishan Bhattacharya <J16N@users.noreply.github.com>
2018-02-06 16:55:13 +0530

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -3,14 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-	int inp,ans=0,ans1,len;
+	int inp,len;
+	long long ans=0,ans1;
 	printf("Triangle Row: ");
 	scanf("%d", &inp);
 	len=inp;
 	for (int i=0; i<inp; i++, len++){
 		ans=pow(10,i)+ans;
 		ans1=pow(ans,2);
-		printf("%*d\n", len, ans1);
+		printf("%*lld\n", len, ans1);
 	}
 	return 0;
 }
