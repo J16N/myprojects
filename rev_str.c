004b22145484a2fbef1817c1e203bbf8e4e9db0c
Jishan Bhattacharya <J16N@users.noreply.github.com>
2018-04-12 18:11:14 +0530

    Add files via upload

---
+++ rev_str.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	// variables declaration
+	char *arr, i = 0, ind = 0;
+	arr = calloc(1, sizeof(char));
+	// dynamic memory allocation, exit out
+	while (*(arr + ind) != '\n'){
+		scanf("%c", (arr + i));
+		ind = i; // recording size
+		i++;
+		arr = realloc(arr, (i + 1) * sizeof(char));
+	}
+	for (i = ind; i > -1; i--)
+		printf("%c", *(arr + i));
+	
+	free(arr);
+	printf("\n");
+	return 0;
+}
