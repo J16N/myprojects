c477944931fd4977eb93e67193880ad3a25d6bb7
Jishan <J16N@users.noreply.github.com>
2018-04-22 14:19:18 +0530

    Update primenum.py

--- primenum.py
+++ primenum.py
@@ -1,21 +1,24 @@
-beg=int(raw_input("Enter the starting number: "))
-end=int(raw_input("Enter the ending number: "))
-if beg<0 or end<=0:
+# A python implementation of primenum.c? NAH..NAH..First I made this program and
+# eventually found that this is slow. So moved to C :)
+
+beg = int(raw_input("Enter the starting number: "))
+end = int(raw_input("Enter the ending number: "))
+if beg < 0 or end <= 0:
 	print "Choose the range from increasing order of positive infinity."
 else:
 	print "So the prime numbers between %d and %d are as follows:" %(beg, end)
-	for i in range(beg,(end+1)):
-		factors=[]
-		divisor=i-1
-		while divisor>0:
-			if i==1:
+	for i in range(beg, (end + 1)):
+		factors = []
+		divisor = i - 1
+		while divisor > 0:
+			if i == 1:
 				factors.append(i)
-				divisor-=1
+				divisor -= 1
 			else:
-				if i%divisor==0:
+				if i % divisor == 0:
 					factors.append(divisor)
-					divisor-=1
+					divisor -= 1
 				else:
-					divisor-=1
-		if len(factors)==1:
+					divisor -= 1
+		if len(factors) == 1:
 			print i
