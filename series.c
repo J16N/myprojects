081a52febb18e702e9f92aa066dd14e7a0badc95
Jishan <J16N@users.noreply.github.com>
2018-04-22 17:15:35 +0530

    Update series.c

--- series.c
+++ series.c
@@ -1,16 +1,21 @@
+/* This program adds all the values of 1^2 + 2^2 + 3^2 + 4^2 + ... + upto n terms
+ * Here n is the input given by the user
+ * Sadly but there is no such story behind this :(
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	int inp, ans, i = 0;
+	int n, ans;
 	printf("No. of terms: ");
-	scanf("%d", &inp);
-	if (inp >= 0){
-	    for (i = 1; i < (inp + 1); i++)
-		    ans = ans + (i * i);
-	    printf("So the answer of the series is %d.\n", ans);
-	} else
-		printf("No. of terms can never be negetive.\n");
+	scanf("%d", &n);
+	if (n < 0)
+		printf("Number of terms can never be negetive.");
+	else {
+		ans = ((n * (n + 1) * ((2 * n) + 1)) / 6);
+		printf("So the addition is %d.", ans);
+	}
 	
 	return 0;
 }
