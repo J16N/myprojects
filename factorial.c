a07d430375bab8788a74e8135bb10c45ad3b5212
Jishan <J16N@users.noreply.github.com>
2018-04-22 13:25:31 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -2,32 +2,24 @@
 
 int factorial(int num)
 {
-	int ans = num;
-	while (num > 1) {
-		num --;
-		ans *= num;
-	}
-	return ans;
+	if (!num)
+		return 1;
+	else
+		return (num * factorial(num - 1)); //recurssion
 }
 
 int main(int argc, char *argv[])
 {
 	int ans;
-	printf("\n---------------"
-		"--------------------\n");
-	printf("THIS PROGRAM WILL "
-		"DO THE FACTORIAL");
-	printf("\n---------------"
-		"--------------------\n");
+	printf("\n-----------------------------------\n");
+	printf("THIS PROGRAM WILL DO THE FACTORIAL");
+	printf("\n-----------------------------------\n");
 	printf("\nFactorial Of: ");
 	scanf("%d", &ans);
 	if (ans >= 1)
-		printf("\nAns:- %d\n\n", 
-			factorial(ans));
+		printf("\nAns:- %d\n\n", factorial(ans));
 	else
-		printf("\nChoose the number"
-			" greater than one or "
-			"equal to one.\n\n");
+		printf("\nChoose the number greater than one or equal to one.\n\n");
 
 	return 0;
 }
